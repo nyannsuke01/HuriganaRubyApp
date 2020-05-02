f5934acd1da08183298c40af9fe5b22c68fedec7
Nyannsuke01 <shingo_373_100_10ve@yahoo.co.jp>
2020-05-02 23:02:49 +0900

    first commit

---
+++ HuriganaRubyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HuriganaRubyApp
+//
+//  Created by user on 2020/05/02.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
