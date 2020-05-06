0cbc1d8ce4b6e4a7fa7a5917d3d403c19b1d3813
Nyannsuke01 <shingo_373_100_10ve@yahoo.co.jp>
2020-05-06 20:10:15 +0900

    UnitTest UITestの実装を行いました

--- HuriganaRubyAppUITests/HuriganaRubyAppUITests.swift
+++ HuriganaRubyAppUITests/HuriganaRubyAppUITests.swift
@@ -12,6 +12,7 @@ class HuriganaRubyAppUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        XCTAssert(XCUIApplication().buttons["convertButton"].exists)
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
@@ -41,3 +42,4 @@ class HuriganaRubyAppUITests: XCTestCase {
         }
     }
 }
+
